456506c6547a9503bbb04be382cfd5d249e42e5a
wyxiao6 <71328511+davidhsiaotw@users.noreply.github.com>
2025-03-11 23:50:25 +0800

    feat: implement missing functions from ChatRepository [a8c4e531]

--- app/src/main/java/com/example/mychatgpt/data/OfflineChatRepository.kt
+++ app/src/main/java/com/example/mychatgpt/data/OfflineChatRepository.kt
@@ -1,15 +1,30 @@
 package com.example.mychatgpt.data
 
 import com.example.mychatgpt.data.dao.ChatroomDao
+import com.example.mychatgpt.data.dao.MessageDao
+import com.example.mychatgpt.data.model.ChatMessage
 import com.example.mychatgpt.data.model.Chatroom
 import kotlinx.coroutines.flow.Flow
 
-class OfflineChatRepository(private val chatroomDao: ChatroomDao) : ChatRepository {
+class OfflineChatRepository(
+    private val chatroomDao: ChatroomDao, private val messageDao: MessageDao
+) : ChatRepository {
     override suspend fun insertChatroom(chatroom: Chatroom) = chatroomDao.insert(chatroom)
 
     override fun getAllChatroom(): Flow<List<Chatroom>> = chatroomDao.getAll()
 
+    override fun getLatestChatroom(): Flow<Chatroom> = chatroomDao.getLatest()
+
     override suspend fun updateChatroom(chatroom: Chatroom) = chatroomDao.update(chatroom)
 
     override suspend fun deleteChatroom(chatroom: Chatroom) = chatroomDao.delete(chatroom)
+
+    override suspend fun insertMessage(chatMessage: ChatMessage) = messageDao.insert(chatMessage)
+
+    override fun getAllMessagesOfChatroom(id: Int): Flow<List<ChatMessage>> =
+        messageDao.getAllByChatroomId(id)
+
+    override suspend fun updateMessage(chatMessage: ChatMessage) = messageDao.update(chatMessage)
+
+    override suspend fun deleteMessage(chatMessage: ChatMessage) = messageDao.delete(chatMessage)
 }
